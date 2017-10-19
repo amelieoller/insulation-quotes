2b33ca143d415e73462ddb7afdca1544e30bb6cb
wicked <github email address>
2017-10-19 11:46:28 -0700

    Create new quote form, show page and controller actions to handle creation

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,32 @@
 class QuotesController < ApplicationController
+
+   def new
+      @quote = Quote.new
+   end
+
+   def create
+      @quote = current_user.quotes.build(quote_params)
+      if @quote.save
+         redirect_to quote_path(@quote)
+      else
+         render :new
+      end
+   end
+
+   def show
+      @quote = Quote.find(params[:id])
+   end
+
+   private
+
+   def quote_params
+      params.require(:quote).permit(
+         :customer_name,
+         :street,
+         :city,
+         :zip_code,
+         :state
+      )
+   end
+
 end
