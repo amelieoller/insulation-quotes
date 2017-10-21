ae1b207f19053342baecdc7263e5b25073f00c1e
wicked <github email address>
2017-10-21 09:46:55 -0700

    Create form helper and move relevant methods into it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,4 @@ module ApplicationHelper
       !!current_user  
    end
 
-   def delete_path(type, model)
-      send("#{model}_path", type)
-   end
-
-   def edit_path(type, model)
-      send("edit_#{model}_path", type)
-   end
-
 end
