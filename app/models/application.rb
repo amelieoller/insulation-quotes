7749883dbbe08d19ca3989857ce25ab49f12b15e
wicked <github email address>
2017-10-24 10:43:35 -0700

    Add validations for applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,10 @@ class Application < ApplicationRecord
   belongs_to :quote  
   has_many :applications_insulation_types
   has_many :insulation_types, through: :applications_insulation_types
+
+  validates :square_foot_price, presence: true
+  validates :description, presence: true
+  validates :length, presence: true
+  validates :height, presence: true
+  
 end
