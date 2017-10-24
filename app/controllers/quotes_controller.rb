57ff1d17c15773bc9b275fe3d3b392e32b89ce03
wicked <github email address>
2017-10-24 10:50:10 -0700

    Abstract calulations and include new database logic

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,21 +1,20 @@
 class QuotesController < ApplicationController
-   before_action :authenticate_user!     
+   before_action :authenticate_user!  
    before_action :find_quote, only: [:show, :edit, :destroy, :update]
-   before_action :calculate_quote, only: [:create]
 
    def new
       @quote = Quote.new
-      2.times{
-         @quote.applications.build
-      }
+      @quote.applications.build
       @insulation_types = InsulationType.all
-      @framing = [{name: "tgi"}, {name: "convent"}]
    end
 
    def create
+      @quote = current_user.quotes.build(quote_params)
       if @quote.save
+         calculate_quote
+         flash[:success] = "Your quote was successfully created!"
          redirect_to quote_path(@quote)
-      else
+      else         
          render :new
       end
    end
@@ -32,12 +31,18 @@ class QuotesController < ApplicationController
 
    def destroy
       @quote.destroy
+      flash[:info] = "Your quote has been deleted!"
       redirect_to quotes_path
    end
 
    def update
-      @quote.update(quote_params)
-      redirect_to quote_path(@quote)
+      if @quote.update(quote_params)
+         calculate_quote
+         flash[:success] = "Your quote was successfully updated!"
+         redirect_to quote_path(@quote)
+      else
+         render :edit
+      end
    end
 
    private
@@ -50,6 +55,8 @@ class QuotesController < ApplicationController
          :zip_code,
          :state,
          applications_attributes: [
+            :_destroy,
+            :id,
             :application_type,
             :description,
             :oc,
@@ -60,7 +67,9 @@ class QuotesController < ApplicationController
             :width,
             :height,
             :vapor_barrier,
-            :square_foot_price
+            :square_foot_price,
+            :insulation_type_ids,
+            :manual_override
          ]
       )
    end
@@ -70,19 +79,19 @@ class QuotesController < ApplicationController
    end
 
    def calculate_quote
-      @quote = current_user.quotes.build(quote_params)
-
-      # Set instance variables
-      @quote.applications.first.attributes.each do |key, value|
-         next if key.include?("id")
-         instance_variable_set("@#{key}", value)
-      end
 
       # Iterate over applications
       @quote.applications.each do |application|
 
+         # Create instance variables and set them equal to the value for each
+         application.attributes.each do |key, value|
+            next if key.include?("id")
+            instance_variable_set("@#{key}", value)
+         end
+
+         # Check Material width
          def material_width
-            if @framing == "conventional" # Conventioal framing
+            if @framing == "Conventional"
                new_width = @oc - 1        
                material_width = new_width unless !InsulationType.where("width == ?", @new_width).empty?
             else
@@ -91,27 +100,26 @@ class QuotesController < ApplicationController
             material_width      
          end
          
+         # Check facing type
          def facing_type
-            if @vapor_barrier == "membrain" || @vapor_barrier == "visqueen"
-               insulation_facing_type = "unfaced"
+            if @vapor_barrier == "Membrain" || @vapor_barrier == "Visqueen"
+               facing = "Unfaced"
             else
-               insulation_facing_type = @vapor_barrier
+               facing = @vapor_barrier
             end
-            insulation_facing_type
+            facing
          end
 
+         # Find the insulation type that fits all the requirements and persit it in the database
          def chosen_insulation
-            InsulationType.where("thickness <= ? AND r_value >= ?", @depth, @r_value).where("width = ?", material_width).where("facing like ?", facing_type).order(price: :asc).first
+            chosen_insulation = InsulationType.where("thickness <= ? AND r_value >= ? AND facing like ?", @depth, @r_value, facing_type).where("width = ? OR width = ?", material_width, @oc)
          end
 
-         def bags_needed
-            if chosen_insulation
-               (@length * @height) / chosen_insulation.coverage.to_i        
-            end
+         if !application.manual_override
+            application.insulation_types = chosen_insulation                
          end
 
-         application.insulation_type = chosen_insulation
-         application.bags_needed = bags_needed
       end
    end
+
 end
