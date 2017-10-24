adb3f24858950891c8b9871f3973c2fb7ddf88fc
wicked <github email address>
2017-10-24 10:46:14 -0700

    Add effective price calculation to quotes helper

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -1,2 +1,14 @@
 module QuotesHelper
+
+   def sorted_by_effective_price(application)
+      effective_price(application).collect {|insulation| insulation}
+   end
+
+   def effective_price(application)
+      application.insulation_types.sort_by do |insulation|
+         price = (insulation.width / application.oc.to_f) * (application.length * application.height) * (insulation.price / insulation.coverage)
+         price
+      end
+   end
+
 end
