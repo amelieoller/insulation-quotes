7ddab5364799283dbdf3e2e737d659f43b08fd8f
wicked <github email address>
2017-10-24 17:33:25 -0700

    Add custom nested attributes writer

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,7 +2,7 @@ class Quote < ApplicationRecord
   belongs_to :user
   has_many :applications, dependent: :destroy
   has_many :insulation_types, through: :applications
-  accepts_nested_attributes_for :applications, allow_destroy: true
+  # accepts_nested_attributes_for :applications, allow_destroy: true
 
   # validates :customer_name, presence: true
   # validates :street, presence: true
@@ -10,10 +10,10 @@ class Quote < ApplicationRecord
   # validates :zip_code, presence: true
   # validates :state, presence: true
 
-  # def applications_attributes=(applications_attributes)
-  #   applications_attributes.each do |i, application_attributes|
-  #     self.applications.build(application_attributes)
-  #   end
-  # end
+  def applications_attributes=(applications_attributes)
+    applications_attributes.each do |_i, application_attributes|
+      self.applications.build(application_attributes)
+    end
+  end
 
 end
