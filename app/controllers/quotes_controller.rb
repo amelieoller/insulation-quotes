8c8c92ed8c8ce4dbf7189af067996500eda61539
amelie <amelieoller@gmail.com>
2017-10-28 14:06:03 -0700

    Move calculate quote logic into the model and add rescue message when no quote is found

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,7 +11,7 @@ class QuotesController < ApplicationController
    def create
       @quote = current_user.quotes.build(quote_params)
       if @quote.save
-         calculate_quote
+         @quote.calculate_quote
          flash[:success] = "Your quote was successfully created!"
          redirect_to quote_path(@quote)
       else         
@@ -37,7 +37,7 @@ class QuotesController < ApplicationController
 
    def update
       if @quote.update(quote_params)
-         calculate_quote
+         @quote.calculate_quote
          flash[:success] = "Your quote was successfully updated!"
          redirect_to quote_path(@quote)
       else
@@ -76,50 +76,9 @@ class QuotesController < ApplicationController
 
    def find_quote
       @quote = Quote.find(params[:id])
-   end
-
-   def calculate_quote
-
-      # Iterate over applications
-      @quote.applications.each do |application|
-
-         # Create instance variables and set them equal to the value for each
-         application.attributes.each do |key, value|
-            next if key.include?("id")
-            instance_variable_set("@#{key}", value)
-         end
-
-         # Check Material width
-         def material_width
-            if @framing == "Conventional"
-               new_width = @oc - 1        
-               material_width = new_width unless !InsulationType.where("width == ?", @new_width).empty?
-            else
-               material_width = @oc
-            end
-            material_width      
-         end
-         
-         # Check facing type
-         def facing_type
-            if @vapor_barrier == "Membrain" || @vapor_barrier == "Visqueen"
-               facing = "Unfaced"
-            else
-               facing = @vapor_barrier
-            end
-            facing
-         end
-
-         # Find the insulation type that fits all the requirements and persit it in the database
-         def chosen_insulation
-            chosen_insulation = InsulationType.where("thickness <= ? AND r_value >= ? AND facing like ?", @depth, @r_value, facing_type).where("width = ? OR width = ?", material_width, @oc)
-         end
-
-         if !application.manual_override
-            application.insulation_types = chosen_insulation                
-         end
-
-      end
+   rescue ActiveRecord::RecordNotFound
+      flash[:info] = "This quote was not found"
+      redirect_to quotes_path
    end
 
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,7 +2,7 @@ class Quote < ApplicationRecord
   belongs_to :user
   has_many :applications, dependent: :destroy
   has_many :insulation_types, through: :applications
-  # accepts_nested_attributes_for :applications, allow_destroy: true
+  accepts_nested_attributes_for :applications, allow_destroy: true
 
   # validates :customer_name, presence: true
   # validates :street, presence: true
@@ -10,10 +10,56 @@ class Quote < ApplicationRecord
   # validates :zip_code, presence: true
   # validates :state, presence: true
 
-  def applications_attributes=(applications_attributes)
-    applications_attributes.each do |_i, application_attributes|
-      self.applications.build(application_attributes)
-    end
-  end
+  # def applications_attributes=(applications_attributes)
+  #   applications_attributes.each do |i, application_attributes|
+  #     self.applications.build(application_attributes)
+  #   end
+  # end
 
+
+  def calculate_quote
+    
+    # Iterate over applications
+    self.applications.each do |application|
+
+      # Create instance variables and set them equal to the value for each
+      application.attributes.each do |key, value|
+        next if key.include?("id")
+        instance_variable_set("@#{key}", value)
+      end
+
+      # Check Material width
+      def material_width
+        if @framing == "Conventional"
+            new_width = @oc - 1        
+            material_width = new_width unless !InsulationType.where("width == ?", @new_width).empty?
+        else
+            material_width = @oc
+        end
+        material_width      
+      end
+      
+      # Check facing type
+      def facing_type
+        if @vapor_barrier == "Membrain" || @vapor_barrier == "Visqueen"
+            facing = "Unfaced"
+        else
+            facing = @vapor_barrier
+        end
+        facing
+      end
+
+      # Find the insulation type that fits all the requirements and persit it in the database
+      def chosen_insulation
+        chosen_insulation = InsulationType.where("thickness <= ? AND r_value >= ? AND facing like ?", @depth, @r_value, facing_type).where("width = ? OR width = ?", material_width, @oc)
+      end
+
+      if !application.manual_override
+        application.insulation_types = chosen_insulation                
+      end
+
+    end
+
+  end
+    
 end
