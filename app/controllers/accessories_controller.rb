1d7b4125ddea74119bbc697ae3a91d6ed64420ed
amelie <amelieoller@gmail.com>
2017-10-31 08:42:18 -0700

    Add index and show actions and views to accessories

---
+++ app/controllers/accessories_controller.rb
@@ -0,0 +1,11 @@
+class AccessoriesController < ApplicationController
+
+   def index
+      @accessories = Accessory.all
+   end
+
+   def show
+      @accessory = Accessory.find(params[:id])
+   end
+
+end
