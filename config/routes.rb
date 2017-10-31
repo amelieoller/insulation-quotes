104a1d166724579774c397d53922c44ca506b23a
amelie <amelieoller@gmail.com>
2017-10-31 13:10:58 -0700

    Add nested index page for accessories

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :accessories
   root 'welcome#home'
   resources :insulation_types
   resources :applications
-  resources :quotes
+  resources :quotes do
+    resources :accessories, only: [:index]    
+  end
+  resources :accessories      
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
