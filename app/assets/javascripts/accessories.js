f7f68fd39d49aafe6578b5680518fc6bcec93649
amelie <amelieoller@gmail.com>
2017-11-30 13:11:32 -0800

    Refactor accessories.js

--- app/assets/javascripts/accessories.js
+++ app/assets/javascripts/accessories.js
@@ -3,41 +3,52 @@ function Accessory(attributes) {
    this.id = attributes.id;
 }
 
-$(function(){
-   if (!!$('#accessory-template').length) {
-      Accessory.source = $('#accessory-template').html()
-      Accessory.template = Handlebars.compile(Accessory.source)
-   }
-})
-
-Accessory.prototype.renderLI = function() {
-   return Accessory.template(this)
+Accessory.success = function(json){
+   $('#accessory_name').val("");
+   
+   var accessory = new Accessory(json);
+   var accessoryLI = accessory.renderLI();
+   
+   $('ul#accessory_list').append(accessoryLI);
 }
 
-$(function() {
-   $('#new_accessory').on('submit', function(e) {
-      e.preventDefault()
-      
-      var $form = $(this)
-      var action = $form.attr('action')
-      var params = $form.serialize()
+Accessory.error = function(response){
+   alert('Looks like this Item already exists');
+}
 
-      $.ajax({
-         url: action,
-         data: params,
-         dataType: 'json',
-         method: 'POST'
-      })
-      .success(function(json){
-         $('#accessory_name').val("")
-         
-         var accessory = new Accessory(json)
-         var accessoryLI = accessory.renderLI()
-         
-         $('ul#accessory_list').append(accessoryLI)
-      })
-      .error(function(response){
-         alert('Looks like this Item already exists')
-      })
+Accessory.formSubmit = function(e) {
+   e.preventDefault();
+   
+   var $form = $(this);
+   var action = $form.attr('action');
+   var params = $form.serialize();
+
+   $.ajax({
+      url: action,
+      data: params,
+      dataType: 'json',
+      method: 'POST'
    })
+   .success(Accessory.success)
+   .error(Accessory.error)
+}
+
+Accessory.formSumbitListener = function() {
+   $('#new_accessory').on('submit', Accessory.formSubmit);
+}
+
+Accessory.ready = function() {
+   Accessory.source = $('#accessory-template').html();
+   Accessory.template = Handlebars.compile(Accessory.source);
+   Accessory.formSumbitListener()
+}
+
+Accessory.prototype.renderLI = function() {
+   return Accessory.template(this);
+}
+
+$(function(){
+   if (!!$('#accessory-template').length) {
+      Accessory.ready()
+   }
 })
