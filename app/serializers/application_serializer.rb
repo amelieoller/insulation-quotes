65aae0ad8f876af0438dbc4f525f564eb2091f4f
amelie <amelieoller@gmail.com>
2017-11-30 14:35:03 -0800

    Add accessory serializer

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,7 +1,7 @@
 class ApplicationSerializer < ActiveModel::Serializer
-  ### Display all columns:
-  # attributes *Application.column_names
-  
-  attributes :id, :insulation_types
-  has_many :insulation_types
+   ### Display all columns:
+   # attributes *Application.column_names
+   
+   attributes :id, :insulation_types
+   has_many :insulation_types
 end
