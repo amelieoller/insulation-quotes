17aea98906b2d89e58b5b3ce4d92d04aa7e98f01
amelie <amelieoller@gmail.com>
2017-11-30 14:35:57 -0800

    Refactor next action to be compatible with insulation types and accessories

--- app/assets/javascripts/insulation_types.js
+++ app/assets/javascripts/insulation_types.js
@@ -1,17 +1,19 @@
 $(function() {
    $('.js-next').click(function(e) {
       var nextId = parseInt($('.js-next').attr('data-id')) + 1;
-      $.getJSON('/insulation_types/' + nextId, function(data) {
-         $('#Name').text(data['name']);
-         $('#Descirption').text(data['description']);         
-         $('#Coverage').text(data['coverage']);
-         $('#R-Value').text(data['r_value']);
-         $('#Thickness').text(data['thickness']);
-         $('#Width').text(data['width']);
-         $('#Facing').text(data['facing']);
-         $('#Price').text(data['price']); 
-         $('a#edit_button').attr('href', `/insulation_types/${nextId}/edit`);
-         $('a#delete_button').attr('href', `/insulation_types/${nextId}`);  
+      url = this.href
+      $.getJSON(url + nextId, function(data) {
+         
+         idArray = $('.select')
+         for (let i = 0; i < idArray.length; i++) {
+            idName = $(idArray[i].id).selector       
+            if (!!idArray[i].href) {
+               $('a#edit_button').attr('href', `/insulation_types/${nextId}/edit`);
+               $('a#delete_button').attr('href', `/insulation_types/${nextId}`);
+            } else {   
+               $(`#${idName}`).text(data[idName]);      
+            }
+         } 
          
          $('.js-next').attr('data-id', data['id'])       
       })
