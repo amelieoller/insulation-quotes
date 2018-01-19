854015dc39372bbc7d426b32544e2972854eaaf8
amelie <amelieoller@gmail.com>
2018-01-19 10:32:07 -0800

    Fix heroku precompile issue

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module InsulationQuotes
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.action_view.automatically_disable_submit_tag = false    
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
